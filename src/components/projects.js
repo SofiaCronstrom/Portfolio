03bc2a378b819c02b2f113f4d2d704c05577b4e2
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-08-02 17:40:34 +0200

    adding structure to about page

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,11 +8,11 @@ return (
 <h1 className="project-head">Projects</h1>
         <div className="container-projects">
             <div className="projOne">1</div>
-            <div className="projTwo">2</div>
-            <div className="projThree">3</div>
-            <div className="projFour">4</div>
-            <div className="projFive">5</div>
-            <div className="projSix">6</div>
+            <div className="projOne">2</div>
+            <div className="projOne">3</div>
+            <div className="projOne">4</div>
+            <div className="projOne">5</div>
+            <div className="projOne">6</div>
         </div>
 </div>
 
