3d921792bf2086d942e5703dd1e81e11a438d800
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-08-06 17:22:50 +0200

    adding grid to contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,15 +10,21 @@ const ContactPage = () =>  {
 return (
 <div className="wrapper-contact">
    <div className="text-contact">
-        <h1>Lets get in touch!</h1>
-        <h2>Email: sofia.cronstrom@gmail.com</h2>
-        <h2>0046704546346</h2>
-        <a href='https://github.com/SofiaCronstrom'>
-        <img src={GithubIcon} className="github" alt="github icon with link to my githubpage when clicked"/>
-        </a>
-        <a href='https://www.linkedin.com/in/sofia-cronstr%C3%B6m-080a4998/'>
-        <img src={LinkedinIcon} alt="linkedin icon with link to my linkedin when clicked"/>
-        </a>
+        <h1>Contact</h1>
+        <div className="text">
+            <h2>sofia.cronstrom@gmail.com</h2>
+            <h2>0046704546346</h2>
+        </div>
+        <div className="gitText">
+            <a href='https://github.com/SofiaCronstrom'>
+            <img src={GithubIcon} className="github" alt="github icon with link to my githubpage when clicked"/>
+            </a> SofiaCronstrom
+        </div>
+        <div className="linkedText">
+            <a href='https://www.linkedin.com/in/sofia-cronstr%C3%B6m-080a4998/'>
+            <img src={LinkedinIcon} className="linkedin" alt="linkedin icon with link to my linkedin when clicked"/>
+            </a> Sofia Cronström
+        </div>
     </div>
 </div>
 
