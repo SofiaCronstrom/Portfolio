0599b07f5b2cc722f5d662178231926998a29b6e
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-08-13 15:02:31 +0200

    adding style to latest module in module-dizzy.js

--- src/components/modal-dizzy.js
+++ src/components/modal-dizzy.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-
+import ImgOne from "../images/modal/aframe-1.svg"
 const Modal = ({isShowing, hide}) => isShowing ? ReactDOM.createPortal(
   <>
     <div className="modal-overlay"/>
@@ -11,9 +11,24 @@ const Modal = ({isShowing, hide}) => isShowing ? ReactDOM.createPortal(
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-       <p>
-         VR game
-       </p>
+         <div className="modal-content">
+          <h2 className="proj-head">webVR/VR game</h2>
+
+          <img src={ImgOne} className="aframe-img" alt="screenshot of the first level of the VR game 'dizzy dreams' pink cylinder on pink background"/>
+
+          <p className="proj-text">
+                Curabitur non erat tempor, finibus sem ut, <br/>
+                cursus tortor. Nullam tristique nisi quis maximus sagittis.<br/>
+                Sed a diam libero. Morbi in purus gravida sapien blandit<br/>
+                volutpat posuere id sem. Curabitur libero felis,<br/> 
+                interdum dapibus mauris quis, sagittis mollis magna.<br/> 
+                Donec egestas feugiat elit, egestas gravida justo hendrerit a.<br/>
+                Duis bibendum eros at eros consectetur, id volutpat<br/> 
+                diam dignissim. Etiam vel mauris est. Vivamus ut erat tincidunt.<br/>
+
+                <a className="proj-link" href="https://sofiacronstrom.github.io/dizzy-dream/">View project</a>
+          </p>
+        </div>
       </div>
     </div>
   </>, document.body
