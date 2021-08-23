b7e7a7204d670b9ba680ff24806de05e847ac5c4
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-08-23 14:21:46 +0200

    changing media queries layout in contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,17 +13,17 @@ return (
         <h1>Contact</h1>
         <div className="text">
             <h2>sofia.cronstrom@gmail.com</h2>
-            <h2>0046704546346</h2>
         </div>
         <div className="gitText">
             <a href='https://github.com/SofiaCronstrom'>
             <img src={GithubIcon} className="github" alt="github icon with link to my githubpage when clicked"/>
-            </a> SofiaCronstrom
-        </div>
-        <div className="linkedText">
+           
+            </a> 
+        
             <a href='https://www.linkedin.com/in/sofia-cronstr%C3%B6m-080a4998/'>
             <img src={LinkedinIcon} className="linkedin" alt="linkedin icon with link to my linkedin when clicked"/>
-            </a> Sofia Cronström
+            
+            </a> 
         </div>
     </div>
 </div>
